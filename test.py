f739e3468b79d3da0c63acce2c149b493d2fdb35
limk <1144650984@qq.com>
2019-04-30 01:27:37 +0800

    q

--- test.py
+++ test.py
@@ -2,25 +2,27 @@ import os
 import tensorflow as tf
 
 print(tf.__version__)
-filepath = "/home/limk/tfrecord/image"
+filepath = "./image"
 pathDir = os.listdir(filepath)
 print(pathDir)
-img_path =pathDir[0]
+img_path = pathDir[0]
 print(img_path)
 
-sess=tf.InteractiveSession()
+img_raw = tf.gfile.FastGFile(filepath + '/' + img_path, mode='rb').read()
+print(repr(img_raw)[:100] + "...")
 
-img_raw = tf.read_file(img_path)
-print(repr(img_raw)[:100]+"...")
+with tf.Session() as sess:
+    img_tensor = tf.image.decode_jpeg(img_raw, channels=3)
 
-img_tensor = tf.image.decode_jpeg(img_raw,channels=3)
+    print(img_tensor)
+    print(img_tensor.dtype)
 
-print(img_tensor.shape)
-print(img_tensor.dtype)
+    img_final = tf.image.resize_images(img_tensor, [192, 192])
+    img_final = img_final / 255.0
 
-img_final = tf.image.resize_images(img_tensor, [192, 192])
-img_final = img_final/255.0
-print(img_final)
-print(img_final.shape)
-print(img_final.numpy().min())
-print(img_final.numpy().max())
+    print(img_final)
+    print(img_final.shape)
+    print(tf.reduce_max(img_final).eval())
+    print(tf.reduce_min(img_final).eval())
+
+
